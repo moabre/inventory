065067c38ab8be6695a3f1487da0a7831fbb089c
moabre <muhammadrehman21@gmail.com>
2021-03-14 02:19:00 -0600

    added the server capability to render static files

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 8080;
+const path = require('path');
 
 const warehouseRoutes = require('./routes/warehouseRoutes');
 const inventoryRoutes = require('./routes/inventoryRoutes');
+
+//Port to listen on
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
 //Express Routes
-app.use("/warehouse", warehouseRoutes);
-app.use("/inventory",inventoryRoutes);
+app.use('/warehouse', warehouseRoutes);
+app.use('/inventory', inventoryRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
 
 app.listen(PORT, () => {
-    console.log(`Express server listening on ${PORT}`);
+  console.log(`Express server listening on ${PORT}`);
 });
